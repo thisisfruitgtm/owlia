a0bf723f4af9966e719a9971e164ab0cc2faa26e
Mihail Marincea <mihailmarincea@gmail.com>
2025-10-15 20:37:41 +0300

    fix: ștergere migrație duplicată add_package_delivery_conditions
    Șters folder-ul vechi 20251015200604_add_package_delivery_conditions (duplicat).
    Păstrez doar 20251015202357_add_package_delivery_conditions cu sintaxa corectată.

--- prisma/migrations/20251015200604_add_package_delivery_conditions/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Package" ADD COLUMN     "deliveryDays" INTEGER,
-ADD COLUMN     "conditions" TEXT,
-ADD COLUMN     "visible" BOOLEAN NOT NULL DEFAULT true;
-
--- Update existing packages to be visible by default
-UPDATE "Package" SET "visible" = true WHERE "visible" IS NULL;
-
